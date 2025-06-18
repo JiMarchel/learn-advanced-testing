27efb0f6c7df8fee394a406b4a155c9da7fdab0f
JiMarchel <jmarchel100@gmail.com>
2025-06-18 16:43:15 +0700

    start

---
+++ exercises/00_intro/00_welcome/src/lib.rs
@@ -0,0 +1,31 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn ctr_is_installed_and_on_path() {
+        std::process::Command::new("cargo")
+            .arg("run")
+            .arg("--package")
+            .arg("ctr")
+            .arg("--bin")
+            .arg("ctr")
+            .output()
+            .expect("Failed to invoke `ctr` through `cargo`");
+    }
+
+    #[test]
+    fn nightly_is_installed() {
+        let output = std::process::Command::new("rustup")
+            .arg("run")
+            .arg("nightly")
+            .arg("cargo")
+            .arg("--version")
+            .output()
+            .expect("Failed to run rustup run nightly cargo --version");
+
+        assert!(
+            output.status.success(),
+            "`rustup run nightly cargo --version` failed: {}\n\nTo fix the error, install the nightly toolchain on your machine:\n\n\trustup toolchain install nightly\n",
+            String::from_utf8_lossy(&output.stderr).trim()
+        );
+    }
+}
